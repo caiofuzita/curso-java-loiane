bea014011686de77c3560e20059c6c740b7b2fdb
Caio Fuzita <caiofuzita@hotmail.com>
2023-08-17 17:07:30 -0300

    Aula 37

---
+++ src/main/java/aula37/Teste.java
@@ -0,0 +1,19 @@
+package aula37;
+
+public class Teste {
+    public static void main(String[] args) {
+    /*
+        Aluno aluno = new Aluno();
+
+        Professor professor = new Professor();
+
+        Pessoa pessoa = new Pessoa();
+
+        aluno.setNome("Caio");
+      */
+
+      Pessoa aluno = new Aluno();
+
+      aluno.setNome("Caio")
+    }
+}
